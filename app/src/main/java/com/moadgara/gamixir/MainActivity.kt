fe5b8753e8a4ad0cd4067a15d26a5ad5539bba6c
YKakdas <ykakdas@gmail.com>
2023-09-19 10:25:40 -0400

    Project setup with splash screen.

---
+++ app/src/main/java/com/moadgara/gamixir/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.moadgara.gamixir
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : AppCompatActivity() {
+
+    private var keepSplashOnScreen = true
+    private val delay = 2000L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        initSplashScreen()
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    private fun initSplashScreen() {
+        val splashScreen = installSplashScreen()
+        splashScreen.setKeepOnScreenCondition { keepSplashOnScreen }
+        Handler(Looper.getMainLooper()).postDelayed({ keepSplashOnScreen = false }, delay)
+    }
+}
